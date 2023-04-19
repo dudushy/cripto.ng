8211b8b49df8aec99b70a2430a27062ff6696737
dudushy <eduardotalarico1337@gmail.com>
2023-04-19 19:04:32 -0300

    Some fun

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -11,7 +11,22 @@ import { DbService } from '../../services/db/db.service';
 export class GameComponent implements OnInit {
   title = 'GameComponent';
 
+  gameRunning = false;
   currentWord = '';
+  gameArray: any = [];
+  wordArray: any = [];
+  letterArray: any = [];
+
+  sampleArray: any = [
+    { word: 'radioterapia', description: 'Técnica empregada no tratamento do câncer.' },
+    { word: 'cafe com leite', description: 'A política que alternou paulistas e mineiros no poder.' },
+    { word: 'gnomo e duende', description: 'Dois seres de contos de fadas.' },
+    { word: 'energia solar', description: 'Aciona os sistemas dos satélites.' },
+    { word: 'ultravioleta', description: 'A radiação que bronzeia e causa câncer.' },
+    { word: 'deselegancia', description: 'Ação indecorosa.' },
+    { word: 'infelicidade', description: 'Desgraça; infortúnio.' },
+    { word: 'aparecimento', description: 'Ato de tornar-se visível.' },
+  ];
 
   constructor(
     public db: DbService,
@@ -19,6 +34,9 @@ export class GameComponent implements OnInit {
     public app: AppComponent
   ) {
     console.log(`[${this.title}#constructor]`);
+
+    this.gameRunning = this.db.get('gameRunning') || false;
+    console.log(`[${this.title}#constructor] gameRunning`, this.gameRunning);
   }
 
   ngOnInit(): void {
@@ -30,12 +48,39 @@ export class GameComponent implements OnInit {
 
       this.currentWord += event.key;
 
-      if (event.key === 'Enter') {
+      if (this.currentWord.length > 12 || event.key === 'Enter') {
         this.currentWord = '';
       }
 
       console.log(`[${this.title}#window.onkeypress] currentWord`, this.currentWord);
     });
+  }
+
+  startGame() {
+    console.log(`[${this.title}#startGame]`);
+
+    this.gameRunning = true;
+    this.db.set('gameRunning', this.gameRunning);
+
+    this.gameArray = this.sampleArray;
+    console.log(`[${this.title}#startGame] gameArray`, this.gameArray);
+
+    // this.wordArray = this.gameArray.map((item) => item.word);
+    console.log(`[${this.title}#startGame] wordArray`, this.wordArray);
+
+    // this.letterArray = this.wordArray.map((word) => word.split(''));
+    console.log(`[${this.title}#startGame] letterArray`, this.letterArray);
+
+    this.updateView();
+  }
+
+  stopGame() {
+    console.log(`[${this.title}#stopGame]`);
+
+    this.gameRunning = false;
+    this.db.set('gameRunning', this.gameRunning);
+
+    this.updateView();
   }
 
   updateView() {
